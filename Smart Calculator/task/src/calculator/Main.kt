bda57f3ac13565799d1efcf80d8335adb2145905
Andrey-Metelyov <andrey.metelyov@gmail.com>
2021-08-15 22:11:27 +0900

    Stage 5/8: Error_

--- Smart Calculator/task/src/calculator/Main.kt
+++ Smart Calculator/task/src/calculator/Main.kt
@@ -4,6 +4,9 @@ fun main() {
     while (true) {
         val input = readLine()!!
         System.err.println("'$input'")
+        if (input.isEmpty()) {
+            continue
+        }
         if (input == "/exit") {
             break
         }
@@ -11,7 +14,8 @@ fun main() {
             println("The program calculates the sum of numbers")
             continue
         }
-        if (input.isEmpty()) {
+        if ("/\\w+".toRegex().matches(input)) {
+            println("Unknown command")
             continue
         }
         val values = input.split(" ")
@@ -21,13 +25,21 @@ fun main() {
         var op = Operation.PLUS
         for (value in values) {
             if (!isOp) {
-                val operand = value.toIntOrNull() ?: 0
+                val operand = value.toIntOrNull()
+                if (operand == null) {
+                    println("Invalid expression")
+                    continue
+                }
                 when (op) {
                     Operation.PLUS -> sum += operand
                     Operation.MINUS -> sum -= operand
                 }
                 System.err.println("sum = $sum")
             } else {
+                if (!"(\\+|\\-)+".toRegex().matches(value)) {
+                    println("Invalid expression")
+                    continue
+                }
                 op = Operation.PLUS
                 for (ch in value) {
                     op = when (ch) {
