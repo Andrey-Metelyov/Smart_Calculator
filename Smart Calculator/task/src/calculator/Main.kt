dd8585599c04fcc2bdea6e3a8b292e1ca653f037
Andrey-Metelyov <andrey.metelyov@gmail.com>
2021-08-15 21:16:32 +0900

    Stage 2/8: 2+2+

--- Smart Calculator/task/src/calculator/Main.kt
+++ Smart Calculator/task/src/calculator/Main.kt
@@ -1,6 +1,23 @@
 package calculator
 
 fun main() {
-    val (a, b) = readLine()!!.split(" ").map { it.toInt() }
-    println(a + b)
+    while (true) {
+        val input = readLine()!!
+        System.err.println("'$input'")
+        if (input == "/exit") {
+            break
+        }
+        if (input.isEmpty()) {
+            continue
+        }
+        val values = input.split(" ")
+        System.err.println("'$values'")
+        val a = values[0].toIntOrNull() ?: 0
+        var b = 0
+        if (values.size > 1) {
+            b = values[1].toIntOrNull() ?: 0
+        }
+        println(a + b)
+    }
+    println("Bye!")
 }
